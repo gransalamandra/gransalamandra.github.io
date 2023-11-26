a38260dbcf07b67344f6667fc68e2d48b564d916
Juan Primo <gransalamandra@gmail.com>
2023-11-26 17:28:57 -0600

    Add files via upload

---
+++ Galvanized_Industries/script.js
@@ -0,0 +1,5 @@
+var typed = new Typed('.typed', {
+  strings: ["Games...................", "Media....................", "Services..................", "We make fantasies realities!"],
+  typeSpeed: 100,
+  contentType: 'html'
+});
